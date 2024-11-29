594eb376649d1c1c01b23971afbc650dd93c249b
Emelie Barthelsson <emeliebarthelsson@gmail.com>
2024-11-29 17:03:18 +0100

    add function to render photos in gallery, add styling and media query of gallery

---
+++ src/js/app.js
@@ -0,0 +1,278 @@
+const images = [
+  {
+    imageUrl: "../assets/images/img1.JPG",
+    watch: "Tudor Black Bay 58",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img2.JPG",
+    watch: "Omega Speedmaster",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img3.JPG",
+    watch: "Tudor Black Bay 58, Omega Speedmaster",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img4.JPG",
+    watch: "Omega Speedmaster",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img5.JPG",
+    watch: "Certina DS Action",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img6.JPG",
+    watch: "KUOE KYOTO Royal Smith",
+    camera: "FUJIFILM X-T4",
+    focalLength: "300mm",
+  },
+  {
+    imageUrl: "../assets/images/img7.JPG",
+    watch: "Omega Speedmaster",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img8.JPG",
+    watch: "Omega Speedmaster",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img9.JPG",
+    watch: "Omega Speedmaster",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img10.JPG",
+    watch: "Maven MUS Solar Chrono",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img11.JPG",
+    watch: "Tag Heuer Professional 3000",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img12.JPG",
+    watch: "Omega Speedmaster",
+    camera: "FUJIFILM X-T4",
+    focalLength: "300mm",
+  },
+  {
+    imageUrl: "../assets/images/img13.JPG",
+    watch: "Omega Speedmaster",
+    camera: "FUJIFILM X-T4",
+    focalLength: "300mm",
+  },
+  {
+    imageUrl: "../assets/images/img14.JPG",
+    watch: "Tudor Black Bay 58",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img15.JPG",
+    watch: "Tusenö Shellback",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img16.JPG",
+    watch: "Tusenö Shellback",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img17.JPG",
+    watch: "Rolex Explorer 1016",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img18.JPG",
+    watch: "Maven MUS Solar Chrono",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img19.JPG",
+    watch: "Moonswatch Mission to Jupiter, Omega Speedmaster, Moonswatch Mission to the moon - July",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img20.JPG",
+    watch: "Oris Diver 68 cotton candy",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img21.JPG",
+    watch: "Omega Speedmaster",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img22.JPG",
+    watch: "Cartier Santos",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img23.JPG",
+    watch: "Breitling Navitimer",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img24.JPG",
+    watch: "Omega Speedmaster",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img25.JPG",
+    watch: "Sphaera Watch Co Desk Diver",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img26.JPG",
+    watch: "Tudor Black Bay 58",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img27.JPG",
+    watch: "Tusenö Shellback",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img28.JPG",
+    watch: "Certina DS Action Diver, Tudor Black Bay 58, Sphaera Watch Co Desk Diver",
+    camera: "FUJIFILM X-T4",
+    focalLength: "37.4mm",
+  },
+  {
+    imageUrl: "../assets/images/img29.JPG",
+    watch: "Oris Diver 68 cotton candy",
+    camera: "FUJIFILM X-T4",
+    focalLength: "300mm",
+  },
+  {
+    imageUrl: "../assets/images/img30.JPG",
+    watch: "Omega Speedmaster",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img31.JPG",
+    watch: "Tudor Black Bay 58",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img32.JPG",
+    watch: "Cartina DS Acion Diver",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img33.JPG",
+    watch: "Tudor Black Bay 58",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img34.JPG",
+    watch: "Maurice Lacroix Aikon Tide",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img35.JPG",
+    watch: "Oris Diver 68 cotton candy",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img36.JPG",
+    watch: "Omega Speedmaster",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img37.JPG",
+    watch: "Yema Superman",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img38.JPG",
+    watch: "Certina DS Action Diver, Tudor Black Bay 58, Sphaera Watch Co Desk Diver",
+    camera: "FUJIFILM X-T4",
+    focalLength: "25.4mm",
+  },
+  {
+    imageUrl: "../assets/images/img39.JPG",
+    watch: "Omega Speedmaster",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+  {
+    imageUrl: "../assets/images/img40.JPG",
+    watch: "Squale 30 Atmos Tropic GMT",
+    camera: "FUJIFILM X-T4",
+    focalLength: "35mm",
+  },
+];
+
+// get element
+const galleryCardContainer = document.querySelector(".gallery-container");
+
+window.addEventListener("DOMContentLoaded", renderGallery(images));
+
+// render gallery
+function renderGallery(images) {
+    galleryCardContainer.textContent = "";
+    images.forEach(image => {
+        // create elements
+        const card = document.createElement("div");
+        card.classList.add("gallery__card");
+
+        const img = document.createElement("img");
+        img.src = image.imageUrl;
+
+        const cardContent = document.createElement("div");
+        cardContent.classList.add("card__content");
+
+        const watch = document.createElement("p");
+        watch.textContent = `Watch: ${image.watch}`;
+
+        const camera = document.createElement("p");
+        camera.textContent = `Camera: ${image.camera}`;
+
+        const focalLength = document.createElement("p");
+        focalLength.textContent = `Focal length: ${image.focalLength}`;
+
+        // append elements
+        cardContent.append(watch, camera, focalLength);
+        card.append(img, cardContent);
+        galleryCardContainer.append(card);
+    });
+};
+
