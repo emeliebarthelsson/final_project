753025c030ae301502a1465fc5c1b24790df981a
Emelie Barthelsson <emeliebarthelsson@gmail.com>
2024-12-09 03:38:02 +0100

    add alt text to images, add search and sort to store

--- src/js/store.js
+++ src/js/store.js
@@ -3,44 +3,51 @@ const items = [
     imageUrl: "../assets/images/img5.JPG",
     watch: "Certina DS Action",
     price: 2000,
-    condition: "Signs of wear"
+    condition: "Signs of wear",
+    altText: "Picture of Certina DS Action",
   },
   {
     imageUrl: "../assets/images/img10.JPG",
     watch: "Maven MUS Solar Chrono",
     price: 2000,
-    condition: "New"
+    condition: "New",
+    altText: "Picture of Maven MUS Solar Chrono",
   },
   {
     imageUrl: "../assets/images/img15.JPG",
     watch: "Tusenö Shellback",
     price: 5000,
-    condition: "Minor signs of wear"
+    condition: "Minor signs of wear",
+    altText: "Picture of Tusenö Shellback",
   },
   {
     imageUrl: "../assets/images/img20.JPG",
     watch: "Oris Diver 65 cotton candy",
     price: 18000,
-    condition: "Minor signs of wear"
+    condition: "Minor signs of wear",
+    altText: "Picture of Oris Diver 65 cotton candy",
   },
   {
     imageUrl: "../assets/images/img32.JPG",
     watch: "Certina DS Acion Diver",
     price: 6000,
-    condition: "Minor signs of wear"
+    condition: "Minor signs of wear",
+    altText: "Picture of Certina DS Acion Diver",
   },
   {
     imageUrl: "../assets/images/img34.JPG",
     watch: "Maurice Lacroix Aikon Tide",
     price: 3000,
-    condition: "Minor signs of wear"
+    condition: "Minor signs of wear",
+    altText: "Picture of Maurice Lacroix Aikon Tide",
   },
   {
     imageUrl: "../assets/images/img37.JPG",
     watch: "Yema Superman",
     price: 5000,
-    condition: "Minor signs of wear"
-  }
+    condition: "Minor signs of wear",
+    altText: "Picture of Yema Superman",
+  },
 ];
 
 // variable
@@ -63,6 +70,7 @@ function renderStore(items) {
 
         const img = document.createElement("img");
         img.src = item.imageUrl;
+        img.alt = item.altText;
 
         const cardContent = document.createElement("div");
         cardContent.classList.add("card__content");
@@ -93,6 +101,43 @@ function renderStore(items) {
     });
 };
 
+// filter
+// get element 
+const search = document.querySelector(".search__input");
+
+search.addEventListener("input", (event) => {
+  let filteredWatches = [...items];
+  filteredWatches = filteredWatches.filter((item) => {
+    return item.watch.toLowerCase().startsWith(event.target.value.toLowerCase());
+  });
+
+  renderStore(filteredWatches);
+});
+
+// sort
+// get element
+const sortButtons = document.querySelectorAll(".sort-button");
+
+const sortWatches = (event) => {
+  let sortedWatches = [...items];
+
+  const sortType = event.target.dataset.sort;
+  if (sortType === "price-low") {
+    sortedWatches = sortedWatches.sort((a, b) => a.price - b.price);
+  } else if (sortType === "price-high") {
+    sortedWatches = sortedWatches.sort((a, b) => b.price - a.price);
+  } else if (sortType === "all") {
+    sortedWatches = [...items];
+  }
+
+  renderStore(sortedWatches);
+};
+
+// event listener to sort buttons
+sortButtons.forEach(button => {
+  button.addEventListener("click", (event) => sortWatches(event));
+});
+
 // cart
 // get elements
 const cart = document.querySelector(".cart");
@@ -113,6 +158,7 @@ function renderCartContent() {
       const contentItemImg = document.createElement("img");
       contentItemImg.classList.add("list-item__img");
       contentItemImg.src = item.imageUrl;
+      contentItemImg.alt = item.altText;
 
       const watchPriceContainer = document.createElement("div");
       watchPriceContainer.classList.add("list-item__description");
